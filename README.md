aaa83d3797b7d83958ba1e0d87302c0337e8ad07
PM02 <pm02@outlook.sa>
2015-02-19 23:58:22 -0500

    SportsData Analysis

--- README.md
+++ README.md
@@ -3,29 +3,29 @@
 -------------
 
 This Repo contains an R script (**run_analysis.R**) on how raw data were cleaned and exported as a dataset.
-The final dataset (**SportDataset.txt**) is the result of the merge of "test" & "train" data.
+The final dataset (**SportDataset.txt**) is the result of the merge of "test" & "train" records.
 
 It also extracts the measurements on the mean & standard deviation for each measuremeant, discarding the rest of the data.
 
-The experiments have been carried out with a group of 30 volunteers within an age bracket of 19-48 years. Each person performed six activities 
-
-(WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING) wearing a smartphone (Samsung Galaxy S II) on the waist.
+The experiments have been carried out with a group of 30 volunteers within an age bracket of 19-48 years. Each person performed six activities (WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING) wearing a smartphone (Samsung Galaxy S II) on the waist.
 
 -------------
 
 ### CodeBook (*SportDataset.txt*):
 Rows: **10299**, Columns: **563**<br>
 Each Row is one observation, Columns are the measurements obtained through the test. 
-* **LabelCode**: Refer to the number of the activity (1 to 6).
-* **LabelName**: Refer to the name of the activity (WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING).
-* **Subject**: Refer to the ID of the volunteer performing the activity.
+* **LabelCode**: Refers to the number of the activity (1 to 6).
+* **LabelName**: Refers to the name of the activity (WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING).
+* **Subject**: Refers to the ID of the volunteer performing the activity.
 * [**All remaining columns**]: Show the mean or standard deviation for a specific measurement used in the test.
 
 -------------
 
 ### How to run_analysis.R:
-Notice: The R script requires *dplyr* library. Step by Step comments are available within the file,
+Notice: The R script requires *dplyr* library. 
+**Step by Step comments are available within the file**
 * Download the data from the link provided below (*end of document*) & copy **run_analysis.R** into the main working directory.
+* Directory path e.g. '*./main/run_analysis.R*' - '*./main/features.txt*' - '*./main/train/...*' - '*./main/test/...*' - ...etc
 * Sit back and relax.
 * Output will be named *SportDataset.txt*, within the same working directory.
 
--- run_analysis.R
+++ run_analysis.R
@@ -1,27 +1,35 @@
 require(dplyr)
+## Folder or subfolder containing the data
+## Default: current working directory
 folder <- "./"
 
+## Read records from the Train Folder
 train_1 <- read.table(paste(folder,"train/subject_train.txt", sep = ""))
 train_x <- read.table(paste(folder,"train/X_train.txt", sep = ""))
 train_y <- read.table(paste(folder,"train/y_train.txt", sep = ""))
 
+## Read records from the Test Folder
 test_1 <- read.table(paste(folder,"test/subject_test.txt", sep = ""))
 test_x <- read.table(paste(folder,"test/X_test.txt", sep = ""))
 test_y <- read.table(paste(folder,"test/y_test.txt", sep = ""))
 
+## Fetch the name of the features (Columns) & names of Labels
 features <- read.table(paste(folder,"features.txt", sep = ""))
 alabels <- read.table(paste(folder,"activity_labels.txt", sep = ""))
 
+## Convert Features column from Factor to Character
+## Clean the names of features and save in new variable 'mrgd'
 features$V2 <- as.character(features$V2)
 mrgd <- features
 mrgd$V2 <- gsub("-",".",mrgd$V2)
 mrgd$V2 <- gsub("\\(\\)","",mrgd$V2)
 
-## Find Columns with Mean or StD only
+## Find Positions of Columns with Mean or Standard Deviation only
 meanstd_list <- grep(mrgd$V2, pattern = "(\\.mean\\.)|(\\.std\\.)", ignore.case = T)
+## Add 2 to the position, since two new columns will bind to the LEFT
 meanstd_list <- meanstd_list + 2;
 
-## Merge Test & Train datasets (Data, Subjects, Labels)
+## Merge Test & Train datasets together (Data, Subjects, Labels)
 indata <- rbind(test_x,train_x)
 subdata <- rbind(test_1,train_1)
 labdata <- rbind(test_y,train_y)
@@ -29,7 +37,7 @@ labdata <- rbind(test_y,train_y)
 ## Assign Column names from Feature List
 colnames(indata) <- mrgd$V2
 
-## Bind Test Labels (labdata) & Subjects (subdata)  
+## Bind Test Labels (labdata) & Subjects (subdata) to the LEFT 
 final <- cbind(subdata$V1, indata)
 final <- cbind(labdata$V1, final)
 
@@ -37,12 +45,14 @@ final <- cbind(labdata$V1, final)
 colnames(final)[2] <- "Subject"
 colnames(final)[1] <- "Label"
 
-## Subset only Means & Standard Deviations
+## Subset only Means & Standard Deviations, from list of positions 'meanstd_list'
 clean_data <- final[,c(1,2,meanstd_list)]
 
-## Merge Label Names
+## Merge Label Names dataframe with the data
+## Match Label Number 'V1' (from list of labels) to Label code (from Cleaned Data)
 finale <- merge(alabels,clean_data, by.y = "Label", by.x="V1")
+## Rename Column names V1, V2 to more descriptive names
 finale <- rename(finale, LabelName = V2, LabelCode = V1)
 
-# Output to file
+# Output to file, without row names.
 write.table(finale, file = "./SportDataset.txt",row.name=FALSE)
